4a00d38cfa961e848d07eccbfeca9a04d2e41d2a
Alpha_1004 <62827961+alphaalt0409@users.noreply.github.com>
2020-09-03 14:27:42 +0900

    Update AntiKickMT.lua

--- AntiKickMT.lua
+++ AntiKickMT.lua
@@ -1,5 +1,7 @@
 local hook = hookfunction or hookfunc
 local newcclosure = newcclosure or protect_function or function(...) return (...) end
+
 hook(game:GetService("Players").LocalPlayer.Kick, newcclosure(function()
-    return warn('Blocked Kick')
+    wait(9e99)
+    return
 end))

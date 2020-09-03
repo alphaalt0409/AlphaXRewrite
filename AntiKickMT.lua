eaebed538185164451ac418311953f00cf07cce8
Alpha_1004 <62827961+alphaalt0409@users.noreply.github.com>
2020-09-03 14:44:30 +0900

    Update AntiKickMT.lua

--- AntiKickMT.lua
+++ AntiKickMT.lua
@@ -1,5 +1,17 @@
 local hook = hookfunction or hookfunc
 local newcclosure = newcclosure or protect_function or function(...) return (...) end
+local mt = getrawmetatable(game)
+local index = mt.__index
+
+setreadonly(mt, false)
+
+hook(mt.__index, newcclosure(function(...)
+      local method = getnamecallmethod()
+      if method:lower() == 'kick' then
+                return
+      end
+      return index(...)    
+end))
 
 hook(game:GetService("Players").LocalPlayer.Kick, newcclosure(function()
     wait(9e99)

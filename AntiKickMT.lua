93f7fd12d542d90333b4c12e8b4bbf137a8c37fb
Alpha_1004 <62827961+alphaalt0409@users.noreply.github.com>
2020-09-07 11:57:17 +0900

    Update AntiKickMT.lua

--- AntiKickMT.lua
+++ AntiKickMT.lua
@@ -1,3 +1,11 @@
-if not getgenv().MTAPIMutex then loadstring(game:HttpGet("https://pastebin.com/raw/UwFCVrhS", true))() end
-game:AddGlobalCallHook("Kick", function(t,...)
-end)
+if getconnections then
+  if getconnections(game.ReplicatedStorage.Interaction:FindFirstChild("Ban") then
+    for i, v in pairs(getconnections(game.ReplicatedStorage.Interaction:FindFirstChild("Ban").AncestryChanged)) do
+      v:Disable()
+    end
+  end
+else
+  if not getgenv().MTAPIMutex then loadstring(game:HttpGet("https://pastebin.com/raw/UwFCVrhS", true))() end
+  game:AddGlobalCallHook("Kick", function(t,...)
+  end)
+end
